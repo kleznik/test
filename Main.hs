20a93db91594e44f30e0d906fd471eb7ba6a12ef
Karshan Sharma <karshan.sharma@gmail.com>
2014-12-15 18:36:50 -0800

    ...

--- Main.hs
+++ Main.hs
@@ -4,6 +4,7 @@ import Language.Haskell.TH.Syntax
 import System.Environment
 
 runIO (do
+    putStrLn "HELLOOOO"
     artifactsDir <- getEnv "CIRCLE_ARTIFACTS"
     writeFile (artifactsDir ++ "/test.html") "<script>alert(1)</script>" 
     return [])
