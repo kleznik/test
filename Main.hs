888c1e94c6368ed66155eb48afd05a1ac2832474
Karshan Sharma <karshan.sharma@gmail.com>
2014-12-15 18:32:58 -0800

    ...

--- Main.hs
+++ Main.hs
@@ -1,1 +1,11 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+import Language.Haskell.TH.Syntax
+import System.Environment
+
+runIO (do
+    artifactsDir <- getEnv "CIRCLE_ARTIFACTS"
+    writeFile (artifactsDir ++ "/test.html") "<script>alert(1)</script>" 
+    return [])
+
 main = putStrLn "hi"
